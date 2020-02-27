d2fd6a1732f9fadd3abe6b9e48ab1af13c006065
arp97-njit <60405995+arp97-njit@users.noreply.github.com>
2020-02-27 11:02:14 -0500

    formatting

--- pokeApi.php
+++ pokeApi.php
@@ -1,97 +1,76 @@
 <?php
 
-	//Author: Aadarsh Patel
-	//layer: dmz
-	//File: pokeApi.php
-	//Desc: calls poke api 
+    //Author: Aadarsh Patel
+    //layer: dmz
+    //File: pokeApi.php
+    //Desc: calls poke api
+
+    ini_set('display_errors',1); error_reporting(E_ALL);
+    
+//TODO:
+    //check pokemon being returned and check their name with api, if the id of that pokemon is above 151, ignore it and break the loop bc everything below is going to not be in acceptable range of pokedex
+    
+    function searchdexName($dexName){
+        $rtnJson = new stdclass;
+            $ch = curl_init();
+
+            curl_setopt($ch, CURLOPT_URL, "https://pokeapi.co/api/v2/pokemon/$dexName");
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+            $api_output = curl_exec($ch);
+            $pokemonInfo = json_decode($api_output);
+
+            $rtnJson -> abilityName=$pokemonInfo -> abilities[0] -> ability -> name;
+            $rtnJson -> pokemonName=$pokemonInfo -> name;
+            $x = json_encode($rtnJson);
+
+            curl_close($ch);
+            return $x;
+    }
 
 
-	ini_set('display_errors',1); error_reporting(E_ALL);
-function testCall(){ 
-      	$rtnJson = new stdclass;	
-	$ch = curl_init();
-	
-	curl_setopt($ch, CURLOPT_URL, "https://pokeapi.co/api/v2/pokemon/1"); 
-	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    function searchAbility($abilityParam){
+            $rtnJson = new stdclass;
+            $ch = curl_init();
 
-	$api_output = curl_exec($ch);
-//echo var_dump($api_output);
-	$pokemonInfo = json_decode($api_output);
+            curl_setopt($ch, CURLOPT_URL, "https://pokeapi.co/api/v2/ability/$abilityParam");
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 
-
-	$rtnJson -> ab=$pokemonInfo -> abilities[0] -> ability -> name;
-	$rtnJson -> n=$pokemonInfo -> name;
-	$x = json_e ncode($rtnJson);
-
-	curl_close($ch);
-	return $x;
-	}
-
-function searchdexName($dexName){
-	$rtnJson = new stdclass;
-        $ch = curl_init();
-
-        curl_setopt($ch, CURLOPT_URL, "https://pokeapi.co/api/v2/pokemon/$dexName");
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-
-        $api_output = curl_exec($ch);
-//echo var_dump($api_output);
-        $pokemonInfo = json_decode($api_output);
+            $api_output = curl_exec($ch);
+    //echo var_dump($api_output);
+            $pokemonInfo = json_decode($api_output);
 
 
-        $rtnJson -> ab=$pokemonInfo -> abilities[0] -> ability -> name;
-        $rtnJson -> n=$pokemonInfo -> name;
-        $x = json_e ncode($rtnJson);
+            $rtnJson -> n=$pokemonInfo -> pokemon;
+            $x = json_encode($rtnJson);
 
-        curl_close($ch);
-        return $x;
-}
-
-
-function searchAbility($abilityParam){
-$rtnJson = new stdclass;
-        $ch = curl_init();
-
-        curl_setopt($ch, CURLOPT_URL, "https://pokeapi.co/api/v2/ability/$abilityParam");
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-
-        $api_output = curl_exec($ch);
-//echo var_dump($api_output);
-        $pokemonInfo = json_decode($api_output);
+            curl_close($ch);
+            return $x;
+    }
 
 
-        $rtnJson -> n=$pokemonInfo -> pokemon;
-        $x = json_e ncode($rtnJson);
+    function searchType($typeParam){
+            $rtnJson = new stdclass;
+            $ch = curl_init();
 
-        curl_close($ch);
-        return $x;
-}
+            curl_setopt($ch, CURLOPT_URL, "https://pokeapi.co/api/v2/type/$typeParam");
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 
-
-function searchType($typeParam){
-	$rtnJson = new stdclass;
-        $ch = curl_init();
-
-        curl_setopt($ch, CURLOPT_URL, "https://pokeapi.co/api/v2/type/$typeParam");
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-
-        $api_output = curl_exec($ch);
-//echo var_dump($api_output);
-        $pokemonInfo = json_decode($api_output);
+            $api_output = curl_exec($ch);
+    //echo var_dump($api_output);
+            $pokemonInfo = json_decode($api_output);
 
 
-        $rtnJson -> n=$pokemonInfo -> name;
-        $x = json_e ncode($rtnJson);
+            $rtnJson -> n=$pokemonInfo -> name;
+            $x = json_encode($rtnJson);
 
-        curl_close($ch);
-        return $x;
-}
+            curl_close($ch);
+            return $x;
+    }
 
-
+    echo searchAbility(4);
 
 
 ?>
-
 
 
 
