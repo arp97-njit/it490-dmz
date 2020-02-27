e1c0ce0a54b6f4c5fb9e9c5f7e5de3e3f17988cf
arp97-njit <60405995+arp97-njit@users.noreply.github.com>
2020-02-27 11:01:18 -0500

    formatting

--- dmzListener.php
+++ dmzListener.php
@@ -1,71 +1,72 @@
 <?php
 
-require_once('/home/aadarsh/Desktop/git/rabbitmqphp_example/path.inc');
-require_once('/home/aadarsh/Desktop/git/rabbitmqphp_example/get_host_info.inc');
-require_once('/home/aadarsh/Desktop/git/rabbitmqphp_example/rabbitMQLib.inc');
-include '/home/aadarsh/Desktop/pokeApi.php';
+    require_once('/home/aadarsh/Desktop/git/rabbitmqphp_example/path.inc');
+    require_once('/home/aadarsh/Desktop/git/rabbitmqphp_example/get_host_info.inc');
+    require_once('/home/aadarsh/Desktop/git/rabbitmqphp_example/rabbitMQLib.inc');
+    include '/home/aadarsh/Desktop/pokeApi.php';
 
 
 
-function requestProcessor($request){
-	echo "Recieved Request".PHP_EOL;
-	echo $request['type'];
-	var_dump($request);
+    function requestProcessor($request){
+        echo "Recieved Request".PHP_EOL;
+        echo $request['type'];
+        var_dump($request);
 
-	if (!isset($request['type'])){
-		return array('message'=>'Error: Message type not set');
-	}
+        if (!isset($request['type'])){
+            return array('message'=>'Error: Message type not set');
+        }
 
-	if($request['type'] == "Search"){ //search for pokemon
-		if(($request['pokemonNum'] == "na") && ($request['name'] == "na")){
-			if($request['pokeType'] == "na"){
-				//type is nothing, check ability
-				if($request['ability'] == "na"){
-					//something is wrong, shouldn't get this far
-					$rspMessage = "example return message";
-				}
-				else {
-					//api call with ability
-					$rspMessage = searchAbility($request['ability']);
-				}
-			}
-			else {
-				//api call with type
-				$rspMessage = searchType($request['pokeType']);
-			}
-		}
-		else {
-			//api call with name/pokedex number
-			if($request['pokemonNum'] != "na"){
-				$rspMessage = searchdexName($request['pokemonNum']);
-			}
-			else{
-				$rspMessage = searchdexName($request['name']);
-			}
-		}
-	}
+        if($request['type'] == "Search") //search for pokemon
+        {
+            if(($request['pokemonNum'] == "na") && ($request['name'] == "na"))
+            { //if true - check type
+                if($request['pokeType'] == "na")
+                {  //if true - check ability
+                    if($request['ability'] == "na")
+                    {
+                        //something is wrong, shouldn't get this far
+                        //may throw an error here?
+                        $rspMessage = "No Search Param - Error";
+                    }
+                    else
+                    {
+                        //api call with ability
+                        $rspMessage = searchAbility($request['ability']);
+                    }
+                }
+                else
+                {
+                    //api call with type
+                    $rspMessage = searchType($request['pokeType']);
+                }
+            }
+            else
+            {
+                //api call with name/pokedex number
+                if($request['pokemonNum'] != "na"){
+                    $rspMessage = searchdexName($request['pokemonNum']);
+                }
+                else{
+                    $rspMessage = searchdexName($request['name']);
+                }
+            }
+        }
+        
+        return $rspMessage;
+
+    }
 
 
-	echo var_dump($rspMessage);
-	return $rspMessage;
-
-}
+    $server = new rabbitMQServer('/home/aadarsh/Desktop/git/rabbitmqphp_example/rabbitMQ_db.ini', 'testServer');
 
 
-//$server = new rabbitMQServer('/home/aadarsh/Desktop/git/rabbitmqphp_example/rabbitMQ_db.ini', 'testServer');
+    $server->process_requests('requestProcessor');
 
-
-//$server->process_requests('requestProcessor');  
-
-$returnedJson = json_decode(testCall());
-$exampleAb = $returnedJson -> ab;
-$exampleN = $returnedJson -> n;
-//this works ^ calls the function in pokeApi.php
-
-echo "name ". $exampleN;
-
+//    $returnedJson = json_decode(testCall());
+//    $exampleAb = $returnedJson -> ab;
+//    $exampleN = $returnedJson -> n;
+    //this works ^ calls the function in pokeApi.php
 
 ?>
-
 
 
